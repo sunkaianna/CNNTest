f2310e2520d0da117d9caa4e33562b5ed9c614eb
sunkaianna <sunkai4u@qq.com>
2014-12-23 18:20:02 +0800

    test

---
+++ cnnlearning/cnnlearning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cnnlearning
+//
+//  Created by sunkai on 14/12/23.
+//  Copyright (c) 2014年 Visgenius. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
